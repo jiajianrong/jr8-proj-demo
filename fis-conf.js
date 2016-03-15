854133f671ad9a30c25ac949391238661c5c435d
孟祥瑞 <429217743@qq.com>
2016-03-15 19:27:46 +0800

    BUG:fe citySelect add

--- fis-conf.js
+++ fis-conf.js
@@ -14,7 +14,7 @@
  * 
  * step 2
  * 编译给Javaer
- * jr8 release -comd ../out-your-path publish
+ * jr8 release -comd ../ publish
  * 
  * 
  * 其他
@@ -22,7 +22,7 @@
  * 2016-02-16
  * 新加性能监控功能
  * jr8 release -cw performance
- * jr8 release -comd ../out-your-path publish performance
+ * jr8 release -comd ../ publish performance
  * 
  * 2016-03-09
  * 整理jr8及插件:
@@ -124,8 +124,8 @@ common_roadmap_path = common_roadmap_path.concat([
     /* 所有其他static静态资源 */
     {
         reg: /^\/static\/(.*)/i,
-        url: '/crm-m/static/$1',
-        release: '/resources/crm-m/static/$1'
+        url: '/resources/static/$1',
+        release: '/resources/static/$1'
     },
     
     /*********************************
@@ -135,14 +135,14 @@ common_roadmap_path = common_roadmap_path.concat([
     // 务必保证次序
     {
         reg: /^\/(page|widget)\/(.+)\.vm$/i,
-        release: '/views/crm-m/$1/$2.html',
+        release: '/views/$1/$2.html',
         isViews: true
     },
 
     {
         reg: /^\/(page|widget)\/(.+)$/i,
-        url: '/crm-m/static/$1/$2',
-        release: '/resources/crm-m/static/$1/$2'
+        url: '/resources/static/$1/$2',
+        release: '/resources/static/$1/$2'
     }
     
 ])
@@ -219,7 +219,7 @@ fis.config.merge({
     // --------------------------------
     deploy: {
         publish: {
-            to: '../output-111'
+            to: '../'
         }
     },
     
---
+++ static/libs/zepto.selectcity.js
@@ -0,0 +1,175 @@
+/**
+ * @authors MXR
+ * @date    2016-02-28
+ * 
+ * demo
+ * var cityList = {
+		list: [
+			{
+				"label":"A",
+	            "value":"key",
+	            "hash": "A"
+			},
+			{
+                "label":"鞍山",
+                "value":"as"
+            }
+		]
+ * };
+ *  var selectcity = $.selectcity({
+		data: cityList,
+		onInit: function () {
+			console.log('init');
+		},
+		onShow: function () {
+			console.log('show');
+		},
+		onMsg: function (data) {
+			console.log('msg');
+			console.log(data);
+		},
+		onHide: function () {
+			console.log('hide');
+		},
+		onDestroy: function () {
+			console.log(88);
+		}
+	});
+	selectcity.show();
+ */
+define( 'libs/zepto.selectcity', function(require, exports, module) {
+
+	var $ = require('zepto'),
+		$body = $('body'),
+		tpl = __inline('./zepto.selectcity.tmpl');
+
+
+	function SelectCity(opts) {
+		//模版渲染
+		var self = this;
+		this.data = opts.data || {};
+		this.$con = $(tpl(this.data));
+		//选中的选项
+		this.selectList = {
+			length: !this.data.checkedVal  ? 0 : this.data.checkedVal.split(',').length
+		};
+
+		this.$con.find(".sl-item-selected").forEach(function(item){
+			var $item = $(item), text = $item.text();
+			self.selectList[text] = $(item).attr('data-sc-value');
+		});
+		// mixin callback
+        ['onInit','onShow','onMsg','onHide','onDestroy'].forEach( $.proxy( function(item){
+            this[item] = opts[item] || function(){};
+        }, this) );
+
+        this.init();
+
+	}
+
+	SelectCity.prototype.init = function () {
+
+		var self = this;
+		var scrop = self.$con;
+		//添加到body
+		$body.append(this.$con);
+
+		//绑定事件
+
+		//侧边栏定位
+		scrop.find('.sc-aside-key').on('click', function (e) {
+			var $e = $(e.target),
+				scrollId = '#' + $e.attr('data-schash'),
+				buffer = $('.sc-top-banner').eq(0).height() || 50,
+				distance = $(scrollId).offset().top - buffer;
+
+			scrollTo(0, distance);
+		});
+		//城市状态切换＋push进选中队列
+		scrop.find('.sl-item').on('click', function (e) {
+			var $e = $(e.target),
+				text = $e.text();
+			//切换状态
+			if (!$e.hasClass('sl-item-selected')) {
+				$e.addClass('sl-item-selected');
+				self.selectList[text] = $e.attr('data-sc-value');
+				++self.selectList.length;
+			} else {
+				$e.removeClass('sl-item-selected');
+				delete self.selectList[text];
+				--self.selectList.length;
+			}
+			//不限和选择的切换
+			if (self.selectList.length === 0) {
+				scrop.find('.sl-limit').addClass('sl-limit-selected');
+				scrop.find('.sc-city-name').text('不限');
+			} else {
+				scrop.find('.sl-limit').removeClass('sl-limit-selected');
+				scrop.find('.sc-city-name').text(self.selectList.length);
+			}
+		});
+		scrop.find('.sl-limit').on('click', function (e) {
+			var $e = $(e.target);
+
+			if ($e.hasClass('sl-limit-selected')) {
+				return
+			} else {
+				$e.addClass('sl-limit-selected');
+				scrop.find('.sl-item').removeClass('sl-item-selected');
+				self.selectList = {
+					length: 0
+				};
+				scrop.find('.sc-city-name').text('不限');
+			}
+		});
+		scrop.find('.sc-city-keep').on('click', function (e) {
+			self.msg();
+		});
+		scrop.find('.sc-top-back').on('click', function (e) {
+			self.hide();
+		});
+
+		this.onInit();
+	}
+
+	SelectCity.prototype.show = function () {
+
+		var self = this;
+
+		self.$con.show();
+
+		this.onShow();
+	}
+
+	SelectCity.prototype.msg = function () {
+
+		var self = this;
+
+		self.onMsg(self.selectList);
+
+		self.hide();
+	}
+
+	SelectCity.prototype.hide = function () {
+
+		var self = this;
+			
+		self.$con.hide();
+
+
+	}
+
+	SelectCity.prototype.destroy = function () {
+
+		var self = this;
+		
+		this.onDestroy();
+	}
+
+	$.selectcity = function (opts) {
+		var selectcity = new SelectCity(opts);
+
+		return selectcity;
+	}
+
+});
